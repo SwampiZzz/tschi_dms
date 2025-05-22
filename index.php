84ced0eb1d0d181cef6165e69e46a663a7ea80f0
SwampiZzz <itzmecj7@gmail.com>
2025-05-22 14:34:58 +0800

    index with pop-up login and sign up

--- index.php
+++ index.php
@@ -8,17 +8,28 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles.css">
   </head>
-  <header>
+  <body>
     <?php
       error_reporting(1);
       session_start();
       include('nav.php');
       nav();
+    ?>
+
+    <section class="hero-section d-flex align-items-center justify-content-center">
+      <div class="text-center">
+        <h1 class="fw-bold display-5">Tanauan School of Craftsmanship and Home Industries<br>Document Management System</h1>
+        <p class="lead">Effortlessly Store, Manage, and Retrieve School Documents.</p>
+        <div class="mt-3">
+          <a class="btn login-btn btn-outline-light me-2" data-bs-toggle="modal" data-bs-target="#login-pop-up">Login now</a>
+          <a class="btn sign-up-btn" data-bs-toggle="modal" data-bs-target="#sign-up-pop-up">Sign up now</a>
+        </div>
+      </div>
+    </section>
+    <?php
       include('login.php');
       include('sign-up.php');
     ?>
-  </header>
-  <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
 </html>
--- nav.php
+++ nav.php
@@ -6,11 +6,11 @@
         <nav class="navbar shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="?nav=home">
-                    <img src="components/logo.png" alt="TSCHI" width="40" height="40">
+                    <img src="elems/logo.png" alt="TSCHI" width="40" height="40">
                 </a>
                 <div>
-                    <a class="btn login-btn text-white" type="button" data-bs-toggle="modal" data-bs-target="#login-pop-up">Login</a>
-                    <button class="btn sign-up-btn me-2" type="button">Sign-Up</button>
+                    <button class="btn login-btn btn-outline-light me-2" type="button" data-bs-toggle="modal" data-bs-target="#login-pop-up">Login</button>
+                    <button class="btn sign-up-btn me-2" type="button" data-bs-toggle="modal" data-bs-target="#sign-up-pop-up">Sign-Up</button>
                 </div>
             </div>
         </nav>
